1f02cf934fcbbe4ec4ff790486a41093e68e8dc0
Jeremy Katras <jakatras@gmail.com>
2019-06-18 16:16:48 -0400

    new Post template; add source-filesystem to plugins

---
+++ src/templates/Post.vue
@@ -0,0 +1,25 @@
+<template>
+    <Layout>
+        <h1>{{ $page.post.title }}</h1>
+        <h2>{{ $page.post.subhead }}
+        <div class="date-published">
+            <p>{{ $page.post }}</p>
+        </div>
+        <div class="featured-image">
+            <g-image v-if="$page.post.featuredImage" :src="$page.post.featuredImage">
+        </div>
+        
+    </Layout>
+</template>
+
+<page-query>
+query Post ($path: String!) {
+    post: post(path: $path) {
+        title
+        path
+        date (format: "D. MMMM YYYY")
+        content
+        featuredImage
+    }
+}
+</page-query>
