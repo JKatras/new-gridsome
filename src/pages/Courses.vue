9a4e5eb79abad3fbd4f3588c88fa5d615da6f06e
Jeremy Katras <jakatras@gmail.com>
2021-07-21 15:56:37 -0400

    add Courses template

--- src/pages/Courses.vue
+++ src/pages/Courses.vue
@@ -3,24 +3,26 @@
     
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
     <!-- <g-image alt="Example image" src="src/favicon.png" width="135" /> -->
-    
-    <h1>Yo, whattup</h1>
-   
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
 
-    <h2>Posts</h2>
+    <h1>Courses</h1>
 
     <div class="posts">
-        <ul>
+        <table>
+            <tr>
+                <th>{{ $page }}</th>
+            <th>
+                <tr>
+                    <td>Date and time</td>
+                    <td>Location</td>
+                </tr>
+            </th>
             <li v-for="edge in $page.posts.edges" :key="edge.node.id" :post="edge.node">
                 <g-link :to="edge.node.path">
                     <h3>{{ edge.node.title }}</h3>
                 </g-link>
                 <p>{{ edge.node.subhead }}</p>
             </li>
-        </ul>
+        </table>
     </div>
 
   </Layout>
